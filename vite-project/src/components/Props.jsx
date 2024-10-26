4b1f693fc74a4159dd24ae63c0723d89630da262
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-10-26 15:39:24 +0530

    De-structuring props

--- vite-project/src/components/Props.jsx
+++ vite-project/src/components/Props.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 
-const Props = (props) => {
-    console.log(props);  // props is object
+// // Normal method
+// const Props = (props) => {
+//     console.log(props);  // props is object
     
-  return (
-    <div>
-        <h1>
-            {props.message} {props.name}
-        </h1>
-    </div>
-  )
+//   return (
+//     <div>
+//         <h1>
+//             {props.message} {props.name}
+//         </h1>
+//     </div>
+//   )
+// }
+
+// // De-structuring Props Method
+// const Props = (props)=>{
+//     const {name,message} = props; // De-structuring props
+//     return(
+//         <div>
+//             <h1>
+//                 {message} {name}
+//             </h1>
+//         </div>
+//     )
+
+// }
+
+// Another De-structuring method
+const Props = ({name,message}) =>{
+    return(
+        <div>
+            <h1>
+            {message} {name}
+            </h1>
+        </div>
+    )
 }
 
 export default Props
