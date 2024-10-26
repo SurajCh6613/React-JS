a9aa3603e3788d90ca3fcb968843c1ab03ac0ede
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-10-26 15:19:34 +0530

    Component Creation and Use

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hello from './components/Hello'
+
+const App = () => {
+  return (
+    <div>
+      {/* Component use */}
+      <Hello /> 
+      
+         
+    </div>
+  )
+}
+
+export default App
